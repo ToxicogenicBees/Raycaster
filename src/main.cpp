69a5d33035ba8a9f7f1221227cca1bc6b6e3dfb0
ToxicogenicBees <tookieboy03@icloud.com>
2025-09-26 10:55:02 -0400

    Renamed Canvas to FrameBuffer for clarrity

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "../include/Rendering/Canvas.h"
+#include "../include/Rendering/FrameBuffer.h"
 #include <random>
 #include <string>
 
@@ -6,15 +6,15 @@ using namespace ImageSize;
 using namespace Colors;
 
 int main(int argc, char *argv[]) {
-    // Resize canvas to desired size
-    canvas.resize(SIZE_1920x1080);
+    // Resize frame buffer to desired size
+    frameBuffer.resize(SIZE_1920x1080);
 
     // Random colors for now
     Color colors[8] = {RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE, WHITE, BLACK};
 
-    for (int x = 0; x < canvas.sizeX(); x++ ) {
-        for (int y = 0; y < canvas.sizeY(); y++) {
-            canvas.setPixel(x, y, colors[std::rand() % 8]);
+    for (int x = 0; x < frameBuffer.sizeX(); x++ ) {
+        for (int y = 0; y < frameBuffer.sizeY(); y++) {
+            frameBuffer.setPixel(x, y, colors[std::rand() % 8]);
         }
     }
 
@@ -22,7 +22,7 @@ int main(int argc, char *argv[]) {
     std::string fileName = (argc > 1 ? argv[1] : "out");
 
     // Render an image
-    canvas.outputToFile(fileName);
+    frameBuffer.outputToFile(fileName);
     
     return 0;
 }
