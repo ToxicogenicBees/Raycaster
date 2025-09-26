7ced80804fcd07860089ab974247f582d464ab84
ToxicogenicBees <tookieboy03@icloud.com>
2025-09-26 10:54:35 -0400

    Renamed Canvas to FrameBuffer for clarrity

--- include/Rendering/FrameBuffer.h
+++ include/Rendering/FrameBuffer.h
@@ -4,23 +4,23 @@
 #include "../Color.h"
 #include <string>
 
-class Canvas {
+class FrameBuffer {
     private:
-        static Vector2<Color> _frame_buffer;
+        static Vector2<Color> _buffer;
 
     public:
         static void outputToFile(const std::string& image_name);
         
         static void setPixel(uint16_t x, uint16_t y, const Color& color);
 
-        static void resize(uint16_t size_x, uint16_t size_y) { _frame_buffer.resize(size_x, size_y); }
-        static void resize(const Point<uint16_t>& size) { _frame_buffer.resize(size); }
-        static void fill(const Color& color) { _frame_buffer.fill(color); }
-        static uint16_t sizeX() { return _frame_buffer.sizeX(); }
-        static uint16_t sizeY() { return _frame_buffer.sizeY(); }
+        static void resize(uint16_t size_x, uint16_t size_y) { _buffer.resize(size_x, size_y); }
+        static void resize(const Point<uint16_t>& size) { _buffer.resize(size); }
+        static void fill(const Color& color) { _buffer.fill(color); }
+        static uint16_t sizeX() { return _buffer.sizeX(); }
+        static uint16_t sizeY() { return _buffer.sizeY(); }
 };
 
-extern Canvas canvas;
+extern FrameBuffer frameBuffer;
 
 namespace ImageSize {
     extern const Point<uint16_t> SIZE_1080x1080;
