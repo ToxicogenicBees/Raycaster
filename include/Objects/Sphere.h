973747ce87a236dd6ae97641be8ae6a549263b94
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2025-09-26 18:22:40 -0400

    Sphere object

--- include/Objects/Sphere.h
+++ include/Objects/Sphere.h
@@ -1,7 +1,14 @@
-#pragma once;
+#pragma once
 
 #include "BaseObject.h"
 
 class Sphere : BaseObject {
+    private:
+        const double _RADIUS;
 
+    public:
+        Sphere(const double3& pos, double radius, const Reflectance& reflectivity);
+        Sphere(double radius, const Reflectance& reflectivity);
+        Sphere(const double3& pos, double radius);
+        Sphere(double radius = 0);
 };
---
+++ src/Objects/Sphere.cpp
@@ -0,0 +1,10 @@
+#include "../../include/Objects/Sphere.h"
+
+namespace {
+    constexpr const char* TYPE = "Sphere";
+}
+
+Sphere::Sphere(const double3& pos, double radius, const Reflectance& reflectivity) : BaseObject(TYPE, pos, reflectivity), _RADIUS(radius) {}
+Sphere::Sphere(double radius, const Reflectance& reflectivity) : BaseObject(TYPE, reflectivity), _RADIUS(radius) {}
+Sphere::Sphere(const double3& pos, double radius) : BaseObject(TYPE, pos), _RADIUS(radius) {}
+Sphere::Sphere(double radius) : BaseObject(TYPE), _RADIUS(radius) {}
