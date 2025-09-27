4019af1110581222feec08231a80d36f23f0ff99
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2025-09-26 22:29:30 -0400

    Sphere drawing example

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,44 @@
 #include "../include/Rendering/FrameBuffer.h"
+#include "../include/Rendering/Camera.h"
+#include "../include/Scene/Objects/Sphere.h"
 #include <random>
 #include <string>
 
 using namespace ImageSize;
 using namespace Colors;
 
-int main(int argc, char *argv[]) {
-    // Resize frame buffer to desired size
-    frameBuffer.resize(SIZE_1920x1080);
+// int main(int argc, char *argv[]) {
+//     // Resize frame buffer to desired size
+//     frameBuffer.resize(SIZE_1920x1080);
 
-    // Random colors for now
-    Color colors[8] = {RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE, WHITE, BLACK};
+//     // Random colors for now
+//     Color colors[8] = {RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE, WHITE, BLACK};
 
-    for (int x = 0; x < frameBuffer.sizeX(); x++ ) {
-        for (int y = 0; y < frameBuffer.sizeY(); y++) {
-            frameBuffer.setPixel(x, y, colors[std::rand() % 8]);
-        }
-    }
+//     for (int x = 0; x < frameBuffer.sizeX(); x++ ) {
+//         for (int y = 0; y < frameBuffer.sizeY(); y++) {
+//             frameBuffer.setPixel(x, y, colors[std::rand() % 8]);
+//         }
+//     }
 
-    // Determine if a file name was passed
-    std::string fileName = (argc > 1 ? argv[1] : "out");
+//     // Determine if a file name was passed
+//     std::string fileName = (argc > 1 ? argv[1] : "out");
 
-    // Render an image
-    frameBuffer.outputToFile(fileName);
+//     // Render an image
+//     frameBuffer.outputToFile(fileName);
     
+//     return 0;
+// }
+
+int main() {
+    // Position camera
+    camera.setPosition(double3(5, 5, 5));
+    camera.setLookVector(double3(0, 0, 0));
+
+    // Place sphere
+    Sphere sphere(double3(0, 0, 0), 1);
+
+    // Cast rays
+    camera.castRays(3.141592 / 4);
+
     return 0;
 }
