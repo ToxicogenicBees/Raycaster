13e922ba48f93985d945a66301f6dce4e9dbabb8
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2025-09-28 02:43:35 -0400

    Converted to struct to match BaseObject

--- include/Scene/Objects/Plane.h
+++ include/Scene/Objects/Plane.h
@@ -2,14 +2,13 @@
 
 #include "BaseObject.h"
 
-class Plane : public BaseObject {
-    public:
-        Plane(double3 normal, double distance);
-        Plane(double distance);
+struct Plane : BaseObject {
+    Plane(double3 normal, double distance);
+    Plane(double distance);
 
-        void translate(double dx, double dy, double dz) override;
-        void rotate(double rx, double ry, double rz) override;
-        void scale(double sx, double sy, double sz) override;
+    void translate(double dx, double dy, double dz) override;
+    void rotate(double rx, double ry, double rz) override;
+    void scale(double sx, double sy, double sz) override;
 
-        Intersection findIntersection(const double3& start, const double3& dir) const override;
+    Intersection findIntersection(const double3& start, const double3& dir) const override;
 };
--- include/Scene/Objects/Sphere.h
+++ include/Scene/Objects/Sphere.h
@@ -2,9 +2,8 @@
 
 #include "BaseObject.h"
 
-class Sphere : public BaseObject {
-    public:
-        Sphere(double radius = 0);
+struct Sphere : BaseObject {
+    Sphere(double radius = 0);
 
-        Intersection findIntersection(const double3& start, const double3& dir) const override;
+    Intersection findIntersection(const double3& start, const double3& dir) const override;
 };
