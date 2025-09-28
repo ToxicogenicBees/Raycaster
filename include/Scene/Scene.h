47913ed1ae00d605c66d8d5bcfb3d140434c00e5
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2025-09-28 01:16:29 -0400

    Text representation of example in main.cpp

--- include/Scene/Scene.h
+++ include/Scene/Scene.h
@@ -1,5 +1,14 @@
 #pragma once
 
-class Scene {
+#include <string>
 
+class Scene {
+    private:
+        static bool _attenuation;
+
+        static void _parseSceneFile(const std::string& file_name);
+
+    public:
+        static bool useAttenuation(bool state);
+        static void renderScene(const std::string& file_name);
 };
