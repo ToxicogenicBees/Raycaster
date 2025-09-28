667a772e1faed4f698952f2621a421679b5234e3
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2025-09-27 23:03:51 -0400

    Better example

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include "../include/Rendering/FrameBuffer.h"
-#include "../include/Rendering/Camera.h"
-#include "../include/Scene/Objects/Sphere.h"
+#include "../include/Scene/BasicObjects.h"
 #include <random>
 #include <string>
 
@@ -9,16 +8,29 @@ using namespace Colors;
 
 int main() {
     frameBuffer.resize(SIZE_1920x1080); // Adjusting image size
+
+    double3 sphere_center(0.0, 0.0, 0.0);
+    double sphere_radius = 2;
     
     // Position camera
-    camera.translate(5, 0, 5);          // Camera's position
-    camera.lookAt(0, 0, 0);             // Point camera's pointing towards
+    camera.translate(sphere_center + double3(5, 5, 5));
+    camera.lookAt(sphere_center);
 
-    // Place sphere
-    Sphere sphere(2);                   // Sphere, centered at origin, with radius 2
+    // Place sphere (radius 2)
+    Sphere sphere(2);
+    sphere.color = Color(1.0, 0.1, 0.1);
+    sphere.specular = 0.9;
+    sphere.shininess = 30;
 
-    // Cast rays
-    camera.render("CameraOutput");      // Rendering the scene
+    // Place plane
+    Plane plane(sphere_center - double3(0, 0, sphere_radius));
+    plane.color = Color(0.0, 1.0, 0.2);
+
+    // Place lights
+    Light light1(double3(1.0, 3.0, 5.0));
+    
+    // Rendering the scene
+    camera.render("CameraOutput");
 
     return 0;
 }
