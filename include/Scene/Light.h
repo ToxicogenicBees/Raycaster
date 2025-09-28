00404efa14502e528adb47269e148ff8bb011eee
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2025-09-27 23:02:33 -0400

    Ambient light intensity is global

--- include/Scene/Light.h
+++ include/Scene/Light.h
@@ -1,26 +1,26 @@
 #pragma once
 
-#include "../Rendering/Reflectance.h"
 #include "../Types/Point.hpp"
 #include <stdio.h>
 #include <vector>
 
 using double3 = Point<double>;
 
-class Light {
-    private:
-        Intensity _int_r, _int_g, _int_b;       // Intensity constants
-        double3 _pos;                           // Position in units
+struct Intensity {
+    double diffusion = 1;
+    double specular = 1;
+};
+
+struct Light {
+    static std::vector<Light*> lights;  // List of all created lights
+    static double ambient_intensity;    // Default ambient intensity
+
+    Intensity int_r, int_g, int_b;      // Intensity constants
+    double3 position;                   // Position in units
     
-    public:
-        static std::vector<Light*> lights;      // List of all created lights
+    Light(const double3& pos, const Intensity& red_intensity, const Intensity& green_intensity, const Intensity& blue_intensity);    
+    Light(const double3& pos = double3(), const Intensity& intensity = Intensity());
 
-        Light(const double3& pos, const Intensity& red_intensity, const Intensity& green_intensity, const Intensity& blue_intensity);    
-        Light(const double3& pos = double3(), const Intensity& intensity = Intensity());
-
-        const Intensity& red() const { return _int_r; }
-        const Intensity& green() const { return _int_g; }
-        const Intensity& blue() const { return _int_b; }
-
-        const double3& position() const { return _pos; }
+    void translate(double dx, double dy, double dz);
+    void translate(const double3& dp);
 };
--- src/Scene/Light.cpp
+++ src/Scene/Light.cpp
@@ -1,17 +1,30 @@
 #include "../../include/Scene/Light.h"
 
+double Light::ambient_intensity = 0.1;
 std::vector<Light*> Light::lights;
 
 Light::Light(const double3& pos, const Intensity& red_intensity, const Intensity& green_intensity, const Intensity& blue_intensity) {
-    _int_r = red_intensity;
-    _int_g = green_intensity;
-    _int_b = blue_intensity;
-    _pos = pos;
+    int_r = red_intensity;
+    int_g = green_intensity;
+    int_b = blue_intensity;
+    position = pos;
 
     lights.push_back(this);
 }
 
 Light::Light(const double3& pos, const Intensity& intensity) {
-    _int_r = _int_g = _int_b = intensity;
-    _pos = pos;
+    int_r = int_g = int_b = intensity;
+    position = pos;
+
+    lights.push_back(this);
+}
+
+void Light::translate(double x, double y, double z) {
+    position.x += x;
+    position.y += y;
+    position.z += z;
+}
+
+void Light::translate(const double3& dp) {
+    position += dp;
 }
