398c82d57ea04afdda724a9e59cfeeaae14e786b
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2025-09-28 20:29:19 -0400

    FrameBuffer is no longer a static class

--- include/Rendering/FrameBuffer.h
+++ include/Rendering/FrameBuffer.h
@@ -1,37 +1,21 @@
 #pragma once
 
+#include "../Rendering/Color.h"
 #include "../Types/Vector2.hpp"
-#include "../Types/Color.h"
 #include <string>
 
 class FrameBuffer {
     private:
-        static Vector2<Color> _buffer;
-
-        static void _gammaCurrection();
+        Vector2<Color> _buffer = Vector2<Color>(1920, 1080);
+        Color _gammaCorrection(const Color& color) const;
 
     public:
-        static void outputToFile(const std::string& image_name);
+        void outputToFile(const std::string& image_name) const;
         
-        static void setPixel(uint16_t x, uint16_t y, const Color& color);
+        void setPixel(uint16_t x, uint16_t y, const Color& color);
+        void resize(uint16_t size_x, uint16_t size_y);
+        void fill(const Color& color);
 
-        static void resize(uint16_t size_x, uint16_t size_y) { _buffer.resize(size_x, size_y); }
-        static void resize(const Point<uint16_t>& size) { _buffer.resize(size); }
-        static void fill(const Color& color) { _buffer.fill(color); }
-        static uint16_t sizeX() { return _buffer.sizeX(); }
-        static uint16_t sizeY() { return _buffer.sizeY(); }
+        uint16_t sizeX() const { return _buffer.sizeX(); }
+        uint16_t sizeY() const { return _buffer.sizeY(); }
 };
-
-extern FrameBuffer frameBuffer;
-
-namespace ImageSize {
-    extern const Point<uint16_t> SIZE_1080x1080;
-    extern const Point<uint16_t> SIZE_1024x768;
-    extern const Point<uint16_t> SIZE_1600x1200;
-    extern const Point<uint16_t> SIZE_1920x1080;
-    extern const Point<uint16_t> SIZE_3840x2160;
-    extern const Point<uint16_t> SIZE_2560x1080;
-    extern const Point<uint16_t> SIZE_1080x1920;
-    extern const Point<uint16_t> SIZE_1280x1024;
-    extern const Point<uint16_t> SIZE_2000x1000;
-}
--- src/Rendering/FrameBuffer.cpp
+++ src/Rendering/FrameBuffer.cpp
@@ -1,60 +1,53 @@
 #include "../../include/Rendering/FrameBuffer.h"
 #include <algorithm>
-#include <random>
 #include <iostream>
 #include <fstream>
 
-namespace ImageSize {
-    const Point<uint16_t> SIZE_1080x1080(1080, 1080);
-    const Point<uint16_t> SIZE_1024x768(1024, 768);
-    const Point<uint16_t> SIZE_1600x1200(1600, 1200);
-    const Point<uint16_t> SIZE_1920x1080(1920, 1080);
-    const Point<uint16_t> SIZE_3840x2160(3840, 2160);
-    const Point<uint16_t> SIZE_2560x1080(2560, 1080);
-    const Point<uint16_t> SIZE_1080x1920(1080, 1920);
-    const Point<uint16_t> SIZE_1280x1024(1280, 1024);
-    const Point<uint16_t> SIZE_2000x1000(2000, 1000);
+Color FrameBuffer::_gammaCorrection(const Color& color) const{
+    return Color(
+        std::clamp(std::pow(color.r, 1.0 / 2.2), 0.0, 1.0),
+        std::clamp(std::pow(color.g, 1.0 / 2.2), 0.0, 1.0),
+        std::clamp(std::pow(color.b, 1.0 / 2.2), 0.0, 1.0)
+    );
 }
 
-Vector2<Color> FrameBuffer::_buffer(ImageSize::SIZE_1080x1080);
-FrameBuffer frameBuffer;
-
-void FrameBuffer::_gammaCurrection() {
-    for (uint16_t y = 0; y < sizeY(); y++) {
-        for (uint16_t x = 0; x < sizeX(); x++) {
-            // Gamma correction
-            _buffer[y][x].r = std::clamp(std::pow(_buffer[y][x].r, 1.0 / 2.2), 0.0, 1.0);
-            _buffer[y][x].g = std::clamp(std::pow(_buffer[y][x].g, 1.0 / 2.2), 0.0, 1.0);
-            _buffer[y][x].b = std::clamp(std::pow(_buffer[y][x].b, 1.0 / 2.2), 0.0, 1.0);
-        } 
-    }
-}
-
-void FrameBuffer::outputToFile(const std::string& image_name) {
+void FrameBuffer::outputToFile(const std::string& image_name) const {
     std::ofstream img((image_name + ".ppm").c_str(), std::ios::binary);
 
     if (!img.is_open()) {
-        std::cout << "Failed to create image " << image_name << std::endl;
+        std::cout << "Failed to create " << image_name << ".ppm\n";
         return;
     }
 
     // File header: P6 = binary RGB
     img << "P6\n" << sizeX() << " " << sizeY() << "\n255\n";
 
-    // Image processing
-    _gammaCurrection();
-
     // Write pixel data
+    Color corrected_color;
+
     for (uint16_t y = 0; y < sizeY(); y++) {
         for (uint16_t x = 0; x < sizeX(); x++) {
-            img.put(255 * _buffer[y][x].r);
-            img.put(255 * _buffer[y][x].g);
-            img.put(255 * _buffer[y][x].g);
+            corrected_color = _gammaCorrection(_buffer[y][x]);
+            
+            img.put(255 * corrected_color.r);
+            img.put(255 * corrected_color.g);
+            img.put(255 * corrected_color.g);
         }
     }
 
+    std::cout << "Created " << image_name << ".ppm\n";
     img.close();
 }
+
+
+void FrameBuffer::resize(uint16_t size_x, uint16_t size_y) {
+    _buffer.resize(size_x, size_y);
+}
+
+ void FrameBuffer::fill(const Color& color) {
+    _buffer.fill(color);
+}
+
 
 void FrameBuffer::setPixel(uint16_t x, uint16_t y, const Color& color) {
     _buffer[y][x] = color;
