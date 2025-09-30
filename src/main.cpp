3bc290f9bf69c364f764150341b040b4bc96fad3
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2025-09-29 22:20:49 -0400

    Updated example

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@ int main(int argc, char **argv) {
     // Place plane
     Plane plane(-2);
     plane.setColor(Color(0, 1, 1));
-    plane.setReflectance(0.05);
+    plane.setReflectance(0.35);
 
     // Place lights
     Light light1;
